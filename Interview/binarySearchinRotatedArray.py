289be86f3f0eb9f6eb784df4aee6b96ebf55870e
gurucharan <gurucharandandyala@gmail.com>
2022-04-22 22:10:58 -0700

    22 april 22

--- Interview/binarySearchinRotatedArray.py
+++ Interview/binarySearchinRotatedArray.py
@@ -1,10 +1,13 @@
 
 # We rotate an ascending order sorted array at some point unknown to user. So for instance, 3 4 5 6 7 might become 5 6 7 3 4. Modify binary search algorithm to find an element in the rotated array in O(log n) time and O(1) Space complexity
 
+# nums = [4,5,6,7,0,1,2], target = 0
+# nums = [4,5,6,7,0,1,2], target = 3
+# nums = [1], target = 0
 
-input = [0,1,2,3,4,5,6,7]
-rotations = 3
-keyToSearch = 7
+input = [4,5,6,7,0,1,2]
+rotations = 0
+keyToSearch = 2
 
 for i in range(0,rotations):
     lastElement = input[len(input)-1]
@@ -46,7 +49,7 @@ while low<=high:
          high = mid-1
         
 
-print("404") if found == False else print( [mid,input[mid]])
+print(-1) if found == False else print( [mid,input[mid]])
     
     
     
